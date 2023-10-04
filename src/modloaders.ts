d489c6cbe4c6407374fa29252f76dc29421eae6f
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2023-10-04 21:38:36 +0200

    Update quilt comments

--- src/modloaders.ts
+++ src/modloaders.ts
@@ -2,8 +2,8 @@ import { ModLoader } from './types';
 
 export const quiltLoader: ModLoader = {
   overrideMods: {
-    P7dR8mSH: 'qvIfYCYJ', // Fabric Api -> QFAPI
-    '308769': '634179', // Fabric Api -> QFAPI
+    P7dR8mSH: 'qvIfYCYJ', // Fabric Api -> QFAPI/QSL
+    '308769': '634179', // Fabric Api -> QFAPI/QSL
 
     Ha28R6CL: 'lwVhp9o5', // Fabric Language Kotlin -> QKL
     '306612': '720410', // Fabric Language Kotlin -> QKL
